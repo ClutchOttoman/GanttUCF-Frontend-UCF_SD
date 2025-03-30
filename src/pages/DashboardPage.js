722b026bf92089084fb51cce90ec873c81c159e8
ShadowBoxing7 <ashkoltz91@gmail.com>
2025-03-30 19:53:56 -0400

    Re-added ToastContainer to Dashboard To-Do List

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -3,11 +3,14 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import DashboardToDo from '../components/DashboardToDo';
 import DashboardNavBar from '../components/DashboardNavBar';
+import {ToastContainer} from 'react-toastify';
 
 const DashboardPage = () =>
 {
     return(
       <div>
+        {/* For toast confirmations and alerts. */}
+        <ToastContainer limit={1}/> 
         <NavBar layout={2}/>
         <DashboardNavBar page={<DashboardToDo/>}/>
       </div>
